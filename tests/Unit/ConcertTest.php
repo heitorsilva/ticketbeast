26d54322a29ee2ffaf55e60ff458450b6c8227f0
Heitor Silva <heitor-silva@hotmail.com>
2018-10-11 22:49:16 +0200

    Make ConcertTest faster

--- tests/Unit/ConcertTest.php
+++ tests/Unit/ConcertTest.php
@@ -4,17 +4,14 @@ namespace Tests\Unit;
 
 use App\Concert;
 use Carbon\Carbon;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 
 class ConcertTest extends TestCase
 {
 
-    use DatabaseMigrations;
-
     public function testCanGetFormattedDate()
     {
-        $concert = factory(Concert::class)->create([
+        $concert = factory(Concert::class)->make([
             'date' => Carbon::parse('2016-12-01 8:00pm')
         ]);
 
@@ -23,7 +20,7 @@ class ConcertTest extends TestCase
 
     public function testCanGetFormattedStartTime()
     {
-        $concert = factory(Concert::class)->create([
+        $concert = factory(Concert::class)->make([
             'date' => Carbon::parse('2016-12-01 17:00:00')
         ]);
         $this->assertEquals('5:00pm', $concert->formatted_start_time);
@@ -31,7 +28,7 @@ class ConcertTest extends TestCase
 
     public function testCanGetTicketPriceInDollars()
     {
-        $concert = factory(Concert::class)->create([
+        $concert = factory(Concert::class)->make([
             'ticket_price' => 6750
         ]);
         $this->assertEquals('67.50', $concert->ticket_price_in_dollars);
