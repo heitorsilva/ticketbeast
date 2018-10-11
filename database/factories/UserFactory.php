a71188ba6cc5d496745f949168373ed8f508f51f
Heitor Silva <heitor-silva@hotmail.com>
2018-10-11 22:33:53 +0200

    Add formatted_date to Concert model

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,5 +1,6 @@
 <?php
 
+use Carbon\Carbon;
 use Faker\Generator as Faker;
 
 /*
@@ -20,5 +21,20 @@ $factory->define(App\User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\Concert::class, function (Faker $faker) {
+    return [
+        'title' => 'Example Band',
+        'subtitle' => 'with The Fake Openers',
+        'date' => Carbon::parse('+2 weeks'),
+        'ticket_price' => 2000,
+        'venue' => 'The Example Theater',
+        'venue_address' => '123 Example Lane',
+        'city' => 'Fakeville',
+        'state' => 'ON',
+        'zip' => '90210',
+        'additional_information' => 'Some sample additional information'
     ];
 });
