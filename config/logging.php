87b7ef2f6daf4d41ce98af2d903ea674a42e5264
Heitor Silva <heitor-silva@hotmail.com>
2018-12-08 18:16:11 +0100

    Add stdout as logging channel option

--- config/logging.php
+++ config/logging.php
@@ -78,6 +78,14 @@ return [
             ],
         ],
 
+        'stdout' => [
+           'driver' => 'monolog',
+           'handler' => StreamHandler::class,
+           'with' => [
+               'stream' => 'php://stdout',
+           ],
+       ],
+
         'syslog' => [
             'driver' => 'syslog',
             'level' => 'debug',
