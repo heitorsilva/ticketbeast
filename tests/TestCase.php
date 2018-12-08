faa7c18e27d3ac1112045dba0a7e4439e7a53d9d
Heitor Silva <heitor-silva@hotmail.com>
2018-12-08 18:46:48 +0100

    Remove disableExcepionHandling
    I'm removing the custom function because
    Laravel 5.6 already comes with
    ->withoutExceptionHandling().

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -10,15 +10,4 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
-
-    protected function disableExceptionHandling() {
-        $this->app->instance(ExceptionHandler::class, new class extends Handler {
-            public function __construct() {}
-            public function report(Exception $exception) {}
-            public function render($request, Exception $exception)
-            {
-                throw $exception;
-            }
-        });
-    }
 }
