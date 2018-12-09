a5d8c0e8d655af46b46afbcd2b59d8094f58dcee
Heitor Silva <heitor-silva@hotmail.com>
2018-12-09 22:53:43 +0100

    Add validation in ConcertOrdersController

--- tests/Feature/PurchaseTicketsTest.php
+++ tests/Feature/PurchaseTicketsTest.php
@@ -13,24 +13,29 @@ class PurchaseTicketsTest extends TestCase
 {
     use DatabaseMigrations;
 
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->paymentGateway = new FakePaymentGateway;
+        $this->app->instance(PaymentGateway::class, $this->paymentGateway);
+    }
+
     public function testCustomerCanPurchaseConcertTickets()
     {
         $this->withoutExceptionHandling();
-
-        $paymentGateway = new FakePaymentGateway;
-        $this->app->instance(PaymentGateway::class, $paymentGateway);
 
         $concert = factory(Concert::class)->create(['ticket_price' => 3250]);
 
         $response = $this->json('POST', "/concerts/{$concert->id}/orders", [
             'email' => 'john@example.com',
             'ticket_quantity' => 3,
-            'payment_token' => $paymentGateway->getValidTestToken()
+            'payment_token' => $this->paymentGateway->getValidTestToken()
         ]);
 
         $response->assertStatus(201);
 
-        $this->assertEquals(9750, $paymentGateway->totalCharges());
+        $this->assertEquals(9750, $this->paymentGateway->totalCharges());
 
         $this->assertTrue($concert->orders->contains(function($order) {
             return $order->email == 'john@example.com';
@@ -39,5 +44,18 @@ class PurchaseTicketsTest extends TestCase
         $order = $concert->orders()->where('email', 'john@example.com')->first();
         $this->assertNotNull($order);
         $this->assertEquals(3, $order->tickets()->count());
+    }
+
+    public function testEmailIsRequiredToPurchaseTickets()
+    {
+        $concert = factory(Concert::class)->create();
+
+        $response = $this->json('POST', "/concerts/{$concert->id}/orders", [
+            'ticket_quantity' => 3,
+            'payment_token' => $this->paymentGateway->getValidTestToken()
+        ]);
+
+        $response->assertStatus(422);
+        $response->assertSee('The email field is required.');
     }
 }
